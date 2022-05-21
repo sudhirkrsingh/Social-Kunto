7607265b1693012f4d77914c176ba7a2a6baf581
sudhirkumar <sudhirkrsingh2740@gmail.com>
2022-05-21 11:00:08 +0530

    1st version commit

---
+++ app/src/main/java/com/mega/socialkunto/Fragment/HomeFragment.java
@@ -0,0 +1,85 @@
+package com.mega.socialkunto.Fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.ArrayRes;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.mega.socialkunto.Adapter.DashboardAdapter;
+import com.mega.socialkunto.Adapter.StoryAdapter;
+import com.mega.socialkunto.Model.DashboardModel;
+import com.mega.socialkunto.Model.StoryModel;
+import com.mega.socialkunto.R;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+
+public class HomeFragment extends Fragment {
+
+    TextView heading;
+    RecyclerView storyRv,dashboardRV;
+    ArrayList<StoryModel> list;
+    ArrayList<DashboardModel> dashboardList;
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_home, container, false);
+
+        heading=view.findViewById(R.id.heading);
+
+
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss a");
+        String formattedDate = df.format(c.getTime());
+
+        if (formattedDate.contains("AM")) {
+            heading.setText("Good Morning");
+        } else {
+            heading.setText("Good Evening");
+        }
+        storyRv=view.findViewById(R.id.storyRV);
+        list= new ArrayList<>();
+
+        list.add(new StoryModel(R.drawable.dua_lipa_profile,R.drawable.live,R.drawable.dua_status,"dua lipa"));
+        list.add(new StoryModel(R.drawable.dua_lipa_profile,R.drawable.live,R.drawable.dua_status,"dua lipa"));
+        list.add(new StoryModel(R.drawable.dua_lipa_profile,R.drawable.live,R.drawable.dua_status,"dua lipa"));
+        list.add(new StoryModel(R.drawable.dua_lipa_profile,R.drawable.live,R.drawable.dua_status,"dua lipa"));
+
+        StoryAdapter adapter = new StoryAdapter(list,getContext());
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false);
+        storyRv.setLayoutManager(linearLayoutManager);
+        storyRv.setNestedScrollingEnabled(false);
+        storyRv.setAdapter(adapter);
+
+
+
+        dashboardRV=view.findViewById(R.id.dashboardRv);
+        dashboardList = new ArrayList<>();
+        dashboardList.add(new DashboardModel(R.drawable.dua_lipa_profile,R.drawable.dua_status,R.drawable.saved,"Dua Lipa","traveler","464","12","15"));
+        dashboardList.add(new DashboardModel(R.drawable.dua_status,R.drawable.dua_profile_img,R.drawable.saved,"Dua Lipa","dancer","530","1223","1533"));
+
+        DashboardAdapter dashboardAdapter = new DashboardAdapter(dashboardList,getContext());
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        dashboardRV.setLayoutManager(layoutManager);
+        dashboardRV.setNestedScrollingEnabled(false);
+        dashboardRV.setAdapter(dashboardAdapter);
+        return view;
+
+    }
+}
---
+++ app/src/main/java/com/mega/socialkunto/MainActivity.java
@@ -0,0 +1,66 @@
+package com.mega.socialkunto;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.Window;
+
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.mega.socialkunto.Fragment.AddFragment;
+import com.mega.socialkunto.Fragment.HomeFragment;
+import com.mega.socialkunto.Fragment.NotificationFragment;
+import com.mega.socialkunto.Fragment.ProfileFragment;
+import com.mega.socialkunto.Fragment.SearchFragment;
+import com.mega.socialkunto.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(R.layout.activity_main);
+
+        Window window = getWindow();
+        window.setBackgroundDrawableResource(R.drawable.gradient_toolbar);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.flFragment,new  HomeFragment()).commit();
+                        return true;
+
+                    case R.id.notification:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.flFragment,new NotificationFragment()).commit();
+                        return true;
+
+                    case R.id.add:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.flFragment,new AddFragment()).commit();
+                        return true;
+                    case R.id.search:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.flFragment,new SearchFragment()).commit();
+                        return true;
+
+                    case R.id.profile:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.flFragment,new ProfileFragment()).commit();
+                        return true;
+
+
+                }
+                return false;
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+    }
+}
